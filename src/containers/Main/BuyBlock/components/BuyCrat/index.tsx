0227057485d598ade7d7ad41fb4098763405ebf5
vKul <valkulchikhin@gmail.com>
2021-09-10 10:48:51 +0300

    fix errors

--- src/containers/Main/BuyBlock/components/BuyCrat/index.tsx
+++ src/containers/Main/BuyBlock/components/BuyCrat/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { FC, useCallback, useState } from 'react';
 import { Text } from 'components/Typography';
 import cx from 'classnames';
--- src/types/global/connected-react-router.d.ts
+++ src/types/global/connected-react-router.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'connected-react-router' {
   import * as React from 'react';
   import { Middleware, Reducer } from 'redux';
